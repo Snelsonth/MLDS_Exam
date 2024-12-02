000004122d583f5169e25a2170ac397be8efdbda
Snelson.code <102351388+Snelsonth@users.noreply.github.com>
2024-12-02 13:05:33 +0100

    Modification package

--- train_classifier.py
+++ train_classifier.py
@@ -1,10 +1,10 @@
+import os
+import joblib
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-import joblib
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 def generate_data():
     X, y = make_classification(
