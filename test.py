bfa623a2c20a3fc1d2382d7ee45b7080aabbb402
Snelson.code <102351388+Snelsonth@users.noreply.github.com>
2024-12-02 10:53:08 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-print('Fichier de test dans main')
-
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
-import joblib
-import matplotlib.pyplot as plt
-import seaborn as sns
