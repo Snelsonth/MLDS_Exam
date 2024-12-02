63c9d348d33ad164d226fac8dfb637941b374ded
Snelson.code <102351388+Snelsonth@users.noreply.github.com>
2024-12-02 10:50:45 +0100

    Importation des library dans le fichier train_classifier.py

---
+++ train_classifier.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
